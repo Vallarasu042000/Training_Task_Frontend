49adba8637a58244a177bc6bc0801eed3906e530
Vallarasu Chinnasamy <15026@chargebackgurus.com>
2023-03-02 10:41:55 +0530

    file added

---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,20 @@
+import { Component, ViewChild } from '@angular/core';
+import { Child1Component } from './child1/child1.component';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent {
+  @ViewChild(Child1Component)
+  private childComponent = {} as Child1Component;
+
+ 
+  changeColor(color:string){
+    this.childComponent.change(color);
+  }
+  demo(){
+   console.log(this.childComponent.test());
+  }
+}
